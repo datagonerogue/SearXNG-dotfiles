f0df9f475a2345a040b0ecdd47d5a6ae343a0b6e
datagonerogue <0221mcsd047@niet.co.in>
2024-05-10 16:57:46 +0200

    add root security

--- secure_ssh.sh
+++ secure_ssh.sh
@@ -2,15 +2,28 @@
 # Make sure to run this script as root
 # Place the public ssh key in "~/.ssh", You can make it using "ssh-keygen -b 4096"
 
-CURRENT_USER=$USER
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Confirmation message with user input
+echo "This script will configure SSH, install and enable a firewall, allow SSH through the firewall, install Fail2Ban, and configure it."
+read -p "Proceed with the script? (y/n): " CONFIRMATION
+
+if [[ $CONFIRMATION != "y" ]]; then
+    echo "aborted."
+    exit 1
+fi
 
 # Add the public key to the new user's authorized_keys file
-sudo mkdir /home/$CURRENT_USER/.ssh
-sudo chmod 700 /home/$CURRENT_USER/.ssh
+sudo mkdir /home/$CURRENT_USER/.ssh && sudo chmod 700 /home/$CURRENT_USER/.ssh
+
 # Edit SSH configuration file to disable root login and limit users who can SSH
 echo "Configuring SSH..."
 {
-      sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_con
+      sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
       echo "AllowUsers $CURRENT_USER" >> /etc/ssh/sshd_config
       systemctl restart sshd
 } > /dev/null 2>&1
